50eff71ede279687daa6a41f757d42e245e465cf
KageuraKoara <128425513+KageuraKoara@users.noreply.github.com>
2026-01-09 21:38:45 +0100

    some changes, i dont rember

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var Main = get_tree().get_root().get_node("Level1")
 @onready var Death = get_parent().find_child("Death")
+@onready var animation_player = $AnimationPlayer
 
 @export var Gravity: float = 800.0
 @export var Jump_Velocity: float = -400.0
@@ -15,7 +16,16 @@ var input_jump: float = 0.0
 var is_jumping: bool = false
 
 func _ready() -> void:
-	pass
+	play_idle()
+
+func play_idle():
+	animation_player.play("Idle")
+
+func play_walking():
+	if velocity.x != 0.0 and self.is_on_floor():
+		animation_player.play("Walking")
+	else:
+		play_idle()
 	
 	#_________ Movement handler _________#
 	
@@ -23,6 +33,7 @@ func _physics_process(delta):
 	move_and_slide()
 	HandleGravity(self, delta)
 	handle_jump(self, jump_input())
+	play_walking()
 
 func _process(_delta: float) -> void:
 	direction = (Death.position - position).normalized()
@@ -38,6 +49,11 @@ func _process(_delta: float) -> void:
 		Speed = 600
 	else:
 		Speed = 300
+	
+	if $SuperJumpTimer.time_left != 0:
+		Jump_Velocity = -600.0
+	else:
+		Jump_Velocity = -400.0
 	
 	input_walking = Input.get_axis("movement_Q", "movement_P")
 	velocity.x = input_walking * Speed
@@ -57,10 +73,29 @@ func HandleGravity (body: CharacterBody2D, delta: float) -> void:
 	if not body.is_on_floor():
 		body.velocity.y += Gravity * delta
 	is_falling = body.velocity.y > 0 and not body.is_on_floor()
+	
+	if is_falling:
+		animation_player.play("Falling")
+	elif is_jumping:
+		animation_player.play("Jumping")
+	else:
+		pass
+	await animation_player.animation_finished
 
 func StartDashTimer():
 	$DashTimer.start()
 
+func StartSJumpTimer():
+	$SuperJumpTimer.start()
+
 func _on_knockback(push):
 	var t = create_tween()
+	animation_player.play("Damage_", randi_range(1, 2))
 	t.tween_property(self, "global_position:x", global_position.x - push, 0.2)
+	await animation_player.animation_finished
+	play_idle()
+
+func play_attack():
+	animation_player.play("Attack_1")
+	await animation_player.animation_finished
+	play_idle()
--- scripts/idle.gd
+++ scripts/idle.gd
@@ -2,16 +2,6 @@ extends State
 
 @onready var collision = $"../../PlayerDetection/CollisionShape2D"
 
-'var player_entered : bool = false
-:
-	set(value):
-		player_entered = value
-		collision.set_deferred("disabled", value)
-
-func _on_player_detected(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		player_entered = true'
-
 func enter():
 	super.enter()
 	animation_player.play("idle")
