4e76c20d9b2b42da78ec3c8697ea87446db365a1
Alina Łucja Osińska <werylilpersona@gmail.com>
2026-01-12 21:24:08 +0100

    Reimporting assets

--- Scripts/Hourglass_top.gd
+++ Scripts/Hourglass_top.gd
@@ -1,7 +1,7 @@
 extends VSlider
+@onready var hourglass: Node2D = $Hourglass
 @onready var bottom: VSlider = $"../Bottom"
 @onready var top: VSlider = $"."
-@onready var hourglass: Node2D = $Hourglass
 
 func _on_value_changed(value: float) -> void:
 	bottom.value = max_value - top.value
