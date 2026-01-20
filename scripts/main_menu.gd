6358f5359a991ca7ac1b762d3241ce2f25c1288b
Alina Łucja Osińska <werylilpersona@gmail.com>
2026-01-20 23:49:34 +0100

    Adding textures

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,4 +1,6 @@
 extends Control
+@onready var key_settings: TextureRect = $Music_sheet/Key_settings
+@onready var key_play: TextureRect = $Music_sheet/Key_play
 
 func _on_play_pressed() -> void:
 	$LevelMenu.visible = true if not $LevelMenu.visible else false
@@ -12,3 +14,17 @@ func _on_credits_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_settings_mouse_entered() -> void:
+	key_settings.visible = true
+
+func _on_settings_mouse_exited() -> void:
+	key_settings.visible = false
+
+
+func _on_play_mouse_entered() -> void:
+	key_play.visible = true
+	
+func _on_play_mouse_exited() -> void:
+	key_play.visible = false
